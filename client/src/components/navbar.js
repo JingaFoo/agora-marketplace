f9a60bbffee442a24654d9e8cc3de81a812b5ea7
JingaFoo <calvinmwariama@yahoo.com>
2018-06-02 05:10:26 -0400

    Update icons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
     <div className="main-nav">
       <nav className="navbar navbar-expand-sm">
         <a className="navbar-brand" href="/"><img src="agora-icon.png" alt="" /></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i className="ion-md-menu"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -14,8 +15,8 @@ const Navbar = () => {
                 <div className="row no-gutters">
                     <div className="col-md-3 col-sm-3">
                         <ul className="navbar-nav">
-                            <li className="nav-item"><a className="nav-item nav-link" href="/categories">Categories</a></li>
-                            <li className="nav-item"><a className="nav-item nav-link nav-border" href="/">Deals</a></li>
+                          <li className="nav-item"><NavLink to="/categories" className="nav-item nav-link">Categories</NavLink></li>
+                          <li className="nav-item"><NavLink to="/" className="nav-item nav-link nav-border">Deals</NavLink></li>
                         </ul>
                     </div>
                     <div className="col-md-9 col-sm-9 ml-auto">
@@ -43,20 +44,20 @@ const Navbar = () => {
             </div>
             <div className="d-none d-sm-block">
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item"><a className="nav-item nav-link active" href="/dashboard">Dashboard</a></li>
-                    <li className="nav-item"><a className="nav-item nav-link nav-border" href="/"><i className="ion-md-settings nav-icon"></i></a></li>
+                  <li className="nav-item"><NavLink to="/" className="nav-item nav-link active">Dashboard</NavLink></li>
+                  <li className="nav-item"><NavLink to="/" className="nav-item nav-link nav-border"><i className="ion-md-settings nav-icon"></i></NavLink></li>
                 </ul>
             </div>
             <div className="nav-dropdown nav-right d-block d-sm-none">
                 <form>
                     <ul className="navbar-nav product-nav">
-                        <li className="nav-item" ><a className="nav-item nav-link shop-links" href="/categories">Categories</a></li>
-                        <li className="nav-item"><a className="nav-item nav-link shop-links" href="/">Deals</a></li>
+                    <li className="nav-item"><NavLink to="/categories" className="nav-item nav-link shop-links">Categories</NavLink></li>
+                    <li className="nav-item"><NavLink to="/" className="nav-item nav-link shop-links">Deals</NavLink></li>
                     </ul>
                     <div className="input-group search-bar">
                         <div className="input-group-append">
                             <button className="btn" type="button">
-                                <i className="fas fa-search"></i>
+                              <i className="ion-md-search nav-icon"></i>
                             </button>
                         </div>
                         <input type="text" className="form-control" placeholder="Search" />
@@ -71,8 +72,8 @@ const Navbar = () => {
                         </div>
                     </div>
                     <ul className="navbar-nav product-nav">
-                        <li className="nav-item"><a className="nav-item nav-link account-links" href="/dashboard">Dashboard</a></li>
-                        <li className="nav-item"><a className="nav-item nav-link account-links" href="/settings">Settings</a></li>
+                    <li className="nav-item"><NavLink to="/" className="nav-item nav-link account-links">Dashboard</NavLink></li>
+                    <li className="nav-item"><NavLink to="/" className="nav-item nav-link account-links">Settings</NavLink></li>
                     </ul>
                 </form>
             </div>
